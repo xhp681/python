806bb068bab5520715430c74c34f779dc14c9d8a
Rince King <22016050+xhp681@users.noreply.github.com>
2022-09-17 12:13:27 +0000

    没其他特殊意义

--- Demo/demo1.py
+++ Demo/demo1.py
@@ -1,3 +1,4 @@
+#%%
 li = [11, 1, 1, 1, 1,  2, 35, 7, 34, 8, 1, 123, 23, 34, 511, 213, 34, 56, 0]
 # 冒泡排序
 def bubbling(arr):
@@ -21,3 +22,4 @@ def bubbling(arr):
 
 new = bubbling(li)
 print(new)
+# %%
